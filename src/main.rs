6ec313fab5dd1b5f44353c731eadc60b922e6e24
lone <45535386+gyl19930522@users.noreply.github.com>
2020-06-03 16:04:14 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use paired::bls12_381::Bls12;
-use generic_array::typenum::U2;
-use neptune::poseidon::PoseidonConstants;
+use generic_array::typenum::{U2, U11};
+use neptune::poseidon::{PoseidonConstants, Arity};
 
-fn main() {
-    let constants = PoseidonConstants::<Bls12, U2>::new();
-    println!("mds_matrices: {:?}, {:?}", constants.mds_matrices.m, constants.mds_matrices.m_hat);
-    println!("len round_constants: {:?}", constants.round_constants.len());
+fn template<A: Arity<Bls12::Fr>>() {
+    let constants = PoseidonConstants::<Bls12, A>::new();
+    println!("mds_matrices: {:?}", constants.mds_matrices.m);
+    print!("*********************************************************");
+    println!("mds_matrices: {:?}", constants.mds_matrices.m_inv);
+    print!("*********************************************************");
+    println!("mds_matrices: {:?}", constants.mds_matrices.m_hat);
+    print!("*********************************************************");
+    println!("mds_matrices: {:?}", constants.mds_matrices.m_hat_inv);
+    print!("*********************************************************");
+    println!("mds_matrices: {:?}", constants.mds_matrices.m_prime);
+    print!("*********************************************************");
+    println!("mds_matrices: {:?}", constants.mds_matrices.m_double_prime);
+    print!("*********************************************************");
     println!("len round_constants: {:?}", constants.round_constants.len());
     println!("len compressed_round_constants: {:?}", constants.compressed_round_constants.len());
     println!("pre_sparse_matrix: {:?}", constants.pre_sparse_matrix);
     println!("len sparse_matrixes: {:?}", constants.sparse_matrixes.len());
     println!("arity_tag: {:?}", constants.arity_tag);
+    println!("full_rounds: {:?}", constants.full_rounds);
+    println!("half_full_rounds: {:?}", constants.half_full_rounds);
+    println!("partial_rounds: {:?}", constants.partial_rounds);
+}
+
+fn main() {
+    template<U2>();
 }
